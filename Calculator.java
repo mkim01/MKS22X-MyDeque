f016dcd577dd0ebd15bf446da651efcfb66fab52
Minuk Kim <mkim01@stuy.edu>
2019-04-02 20:19:18 -0400

    performing operator - should fix fomatString error

--- Calculator.java
+++ Calculator.java
@@ -5,19 +5,47 @@ public class Calculator{
        */
   public static double eval(String s){
     String[] exp = s.split(" ");
-    MyDeque<Double> stack = new MYDeque<Double>();
+    MyDeque<Double> stack = new MyDeque<Double>();
     for (int i = 0; i < exp.length; i++){
-      if (exp[i].equals("+") || exp[i].equals("+") || exp[i].equals("+")|| exp[i].equals("+")){
+      if (exp[i].equals("+") || exp[i].equals("-") || exp[i].equals("/")|| exp[i].equals("%")){
         Double operator = Double.parseDouble(exp[i]);
         stack.addLast(operator);
-      // else{
-      //   if(exp[i].equals)
-      // }
-
-    }
-
+      }
+      else{
+        if(exp[i].equals("+")){
+          double s2 = stack.getLast();
+          stack.removeLast();
+          double s1 = stack.getLast();
+          stack.removeLast();
+          stack.addLast(s1 + s1);
+        }
+        else if (exp[i].equals("-")){
+          double s2 = stack.getLast();
+          stack.removeLast();
+          double s1 = stack.getLast();
+          stack.removeLast();
+          stack.addLast(s1 - s2);
+        }
+        else if (exp[i].equals("/")){
+          double s2 = stack.getLast();
+          stack.removeLast();
+          double s1 = stack.getLast();
+          stack.removeLast();
+          stack.addLast(s1 / s2);
+        }
+        else if (exp[i].equals("%")){
+          double s2 = stack.getLast();
+          stack.removeLast();
+          double s1 = stack.getLast();
+          stack.removeLast();
+          stack.addLast(s1 % s2);
+          }
+        }
+      }
       return stack.getLast();
     }
 
   public static void main(String[] args){
+    System.out.println(eval("5 6.0 +"));
   }
+}
