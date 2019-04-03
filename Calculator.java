96c25eed6aa10e78f4f4b4b61a8eca4956bc2c57
Minuk Kim <mkim01@stuy.edu>
2019-04-02 20:51:06 -0400

    working calculator- fixed performing operation

--- Calculator.java
+++ Calculator.java
@@ -7,7 +7,7 @@ public class Calculator{
     String[] exp = s.split(" ");
     MyDeque<Double> stack = new MyDeque<Double>();
     for (int i = 0; i < exp.length; i++){
-      if (!(exp[i].equals("+") || exp[i].equals("-") || exp[i].equals("/")|| exp[i].equals("%"))){
+      if (!(exp[i].equals("+") || exp[i].equals("-") ||  exp[i].equals("*") || exp[i].equals("/")|| exp[i].equals("%"))){
         Double number = Double.parseDouble(exp[i]);
         stack.addLast(number);
       }
@@ -17,7 +17,7 @@ public class Calculator{
           stack.removeLast();
           double s1 = stack.getLast();
           stack.removeLast();
-          stack.addLast(s1 + s1);
+          stack.addLast(s1 + s2);
         }
         else if (exp[i].equals("-")){
           double s2 = stack.getLast();
@@ -39,13 +39,15 @@ public class Calculator{
           double s1 = stack.getLast();
           stack.removeLast();
           stack.addLast(s1 % s2);
-          }
-        else{
-          double s2 = stack.getLast();
-          stack.removeLast();
-          double s1 = stack.getLast();
-          stack.removeLast();
-          stack.addLast(s1 * s2);
+        }
+          else if (exp[i].equals("*")){
+            double s2 = stack.getLast();
+            stack.removeLast();
+            double s1 = stack.getLast();
+            stack.removeLast();
+            stack.addLast(s1 * s2);
+            }
+          else{;
           }
         }
       }
@@ -54,5 +56,6 @@ public class Calculator{
 
   public static void main(String[] args){
     System.out.println(eval("10 3 +"));
+    System.out.println(eval("1 2 3 4 5 + * - -"));
   }
 }
